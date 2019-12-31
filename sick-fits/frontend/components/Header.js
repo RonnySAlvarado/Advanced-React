274c031c25617e921f01986ca766496122324003
Ronny Alvarado <rsalvarado777@gmail.com>
2019-12-31 14:14:15 -0600

    Styled up page; included _documents.js file to have server-side collect the styles before the client-side renders (basically preventing it from briefly showing the page without css for a quick second). Also included the NProgress package to show loading state with Next/Router

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -5,6 +5,24 @@ import styled from "styled-components";
 
 import Nav from "./Nav";
 
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = () => {
+  console.log("onRouteChangeStart Triggered");
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  console.log("onRouteChangeComplete Triggered");
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  console.log("onRouteChangeError Triggered");
+  NProgress.done();
+};
+
 const Logo = styled.h1`
   font-size: 4rem;
   margin-left: 2rem;
