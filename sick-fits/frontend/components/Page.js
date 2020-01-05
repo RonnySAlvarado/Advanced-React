9dd374f81672daa83adcffe41308c9cc435fdb2c
Ronny Alvarado <rsalvarado777@gmail.com>
2020-01-04 18:20:38 -0600

    Connected front-end to query backend and updated styling

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import styled, { ThemeProvider, injectGlobal } from "styled-components";
-
 import Header from "./Header";
 import Meta from "./Meta";
+
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
 
 const StyledPage = styled.div`
   background: white;
@@ -13,24 +22,12 @@ const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
-  background: ${props => props.theme.red};
 `;
-
-const theme = {
-  red: "#FF0000",
-  black: "#393939",
-  grey: "#3A3A3A",
-  lightgrey: "#E1E1E1",
-  offWhite: "#EDEDED",
-  maxWidth: "1000px",
-  bs: "0 12px 24px 0 rgba(0,0,0,0.09)"
-};
 
 injectGlobal`
   @font-face {
     font-family: 'radnika_next';
-    src: url('/static/radnikanext-medium-webfront.woff2')
-    format('woff2');
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
   }
@@ -50,20 +47,23 @@ injectGlobal`
   }
   a {
     text-decoration: none;
-    color: ${theme.black}
+    color: ${theme.black};
   }
+  button {  font-family: 'radnika_next'; }
 `;
 
-const Page = props => {
-  return (
-    <ThemeProvider theme={theme}>
-      <StyledPage>
-        <Meta />
-        <Header />
-        <Inner>{props.children}</Inner>
-      </StyledPage>
-    </ThemeProvider>
-  );
-};
+class Page extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
+}
 
 export default Page;
