dd2162c0de970606a1f49488fdb269df9abd7374
Ronny Alvarado <rsalvarado777@gmail.com>
2020-01-27 17:14:14 -0600

    Created a gated sign in component

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import CreateItem from "../components/CreateItem";
+import PleaseSignIn from "../components/PleaseSignIn";
 
 const Sell = props => {
   return (
     <div>
-      <CreateItem />
+      <PleaseSignIn>
+        <CreateItem />
+      </PleaseSignIn>
     </div>
   );
 };
